225b9e440257034f0370d7a0984cf09aefa47972
Toon <108662179+ToonMath@users.noreply.github.com>
2022-10-27 17:47:56 +0200

    Included some bash script code
    Bash script code included to track the progression of the calculations

--- ant_hill.sh
+++ ant_hill.sh
@@ -1,8 +1,22 @@
-g++ -O2 -Wall -Werror -Dcircle=1 -pedantic -std=c++14 -o ant_food ant_food.cpp -lm
-./ant_food 1000000
+echo "Give number of searches"
+read Nsearches
+echo "----------------------------------------------------------------------"
+echo "--------------------------circle boundary-----------------------------"
+echo "----------------------------------------------------------------------"
 
-g++ -O2 -Wall -Werror -Dbox=1 -pedantic -std=c++14 -o ant_food ant_food.cpp -lm
-./ant_food 1000000
+g++ -O2 -Wall -Werror -Dcircle=1  -pedantic -std=c++14 -o ant_food ant_food.cpp -lm
+./ant_food $Nsearches
 
-g++ -O2 -Wall -Werror -Dline=1 -pedantic -std=c++14 -o ant_food ant_food.cpp -lm
-./ant_food 1000000
+echo "----------------------------------------------------------------------"
+echo "---------------------------box boundary-------------------------------"
+echo "----------------------------------------------------------------------"
+
+g++ -O2 -Wall -Werror -Dbox=1  -pedantic -std=c++14 -o ant_food ant_food.cpp -lm
+./ant_food $Nsearches
+
+echo "----------------------------------------------------------------------"
+echo "------------------------Line boundary---------------------------------"
+echo "----------------------------------------------------------------------"
+
+g++ -O2 -Wall -Werror -Dline=1  -pedantic -std=c++14 -o ant_food ant_food.cpp -lm
+./ant_food $Nsearches
